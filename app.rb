95f0d27f23f6a24ad70e5ed79469a24c89624ec6
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-17 15:58:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
     "Goodbye, #{@name}."
   end
   
-  get "/multiply" do 
+  get "/multiply/:num1/:num2" do 
     @num1 = params[:num1]
     @num2 = params[:num2]
     @product = @num1 * @num2
